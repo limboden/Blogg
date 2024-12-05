5263e1158f3f5d06e4641c95b9edf9d5ef596c57
limboden <imbodenlu@gmail.com>
2024-12-04 23:35:49 -0500

    working through logic.js more

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,9 +1,9 @@
 // TODO: Create logic to toggle the light/dark mode styles for the page and circle. The mode should be saved to local storage.
-
+localStorage.setItem("lightMode", JSON.stringify(false))
 
 // TODO: Create a function called `readLocalStorage` that reads from local storage and returns the data. If no data exists, return an empty array.
-function readLocalStorage() {
-  const mode = localStorage.getItem("lightDarkMode");
+function readLocalStorage(str) {
+  const mode = localStorage.getItem(str);
   if (mode) {
     return JSON.parse(mode)
   } else {
@@ -13,7 +13,7 @@ function readLocalStorage() {
 
 // TODO: Create a function called `storeLocalStorage` that takes a given object and saves the new data to the existing blog data in local storage.
 function storeLocalStorage(object) {
-
+  localStorage.setItem("blogData", JSON.stringify(object))
 }
 
 // ! Use the following function whenever you need to redirect to a different page
