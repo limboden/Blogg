576db5fd27aabdaf6e68734b2ee969429c870fe0
limboden <imbodenlu@gmail.com>
2024-12-04 23:47:21 -0500

    working on form.js too now

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,11 +1,23 @@
 // TODO: Create a variable that selects the main element, and a variable that selects the back button element
-
+const mainElement = document.querySelector("main")
+const backButtonElement = document.querySelector(".backbutton")
 // TODO: Create a function that builds an element and appends it to the DOM
-
+function buildAndAppendElement() {
+  // ????
+}
 // TODO: Create a function that handles the case where there are no blog posts to display
-
+function noBlogPosts() {
+  // ??
+}
 // TODO: Create a function called `renderBlogList` that renders the list of blog posts if they exist. If not, call the no posts function.
-
+function renderBlogList() {
+  if (true) {// ?
+    // ?
+  } else {
+    noBlogPosts()
+  }
+}
 // TODO: Call the `renderBlogList` function
-
+renderBlogList()
 // TODO: Redirect to the home page using the `redirectPage` function found in logic.js when the back button is clicked
+backButtonElement.addEventListener(click, redirectPage('./form.js')) // maybe
--- assets/js/form.js
+++ assets/js/form.js
@@ -1,5 +1,8 @@
 // TODO: Create a variable that selects the form element
-
+const formElement = document.querySelector("form");
 // TODO: Create a function that handles the form submission. Grab the form data and store it in local storage, then redirect to the blog page using the `redirectPage` function. If the form is submitted with missing data, display an error message to the user.
-
+function handleFormSubmission() {
+  //formElement.children ?
+  // formElement.
+}
 // TODO: Add an event listener to the form on submit. Call the function to handle the form submission.
