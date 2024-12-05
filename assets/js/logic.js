7986a48e8bf04063b3c7a3627a2eabd6f5083398
limboden <imbodenlu@gmail.com>
2024-12-04 23:32:15 -0500

    working through logic.js

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,10 +2,19 @@
 
 
 // TODO: Create a function called `readLocalStorage` that reads from local storage and returns the data. If no data exists, return an empty array.
-
+function readLocalStorage() {
+  const mode = localStorage.getItem("lightDarkMode");
+  if (mode) {
+    return JSON.parse(mode)
+  } else {
+    return []
+  }
+}
 
 // TODO: Create a function called `storeLocalStorage` that takes a given object and saves the new data to the existing blog data in local storage.
+function storeLocalStorage(object) {
 
+}
 
 // ! Use the following function whenever you need to redirect to a different page
 
